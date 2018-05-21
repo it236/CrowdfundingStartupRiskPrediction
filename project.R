abd6d9f3efb9b126d81a4de05123ba80940b0114
Ishaan Tiwari <35909349+it236@users.noreply.github.com>
2018-05-21 10:09:29 -0400

    Update project.R

--- project.R
+++ project.R
@@ -52,6 +52,40 @@ sum(is.factor(data))
 
 data <- data[,!(colnames(data) %in% c("category","main_category","state","country"))]
 
+#Converting all int types to numeric to make them compatible with PCA
+#and creating new data set with no state variable
+data$campLength <- as.numeric(data$campLength)
+data$nameLength <- as.numeric(data$nameLength)
+data$backers <- as.numeric(data$backers)
+
+data2_success <- data[data$state %in% c("successful"),colnames(data) != "state"]
+
+train_ind1 <- sample(1:nrow(data2_success), (4/5)*nrow(data2_success))
+data2_success_normal <- data.frame(scale(data2_success[train_ind1,colnames(data2_success) != "usd_pledged_real"]))
+data2_success_normal$usd_pledged_real <- data2_success$usd_pledged_real[train_ind1]
+data2_success_normal_test <- data.frame(scale(data2_success[-train_ind1,colnames(data2_success) != "usd_pledged_real"]))
+data2_success_normal_test$usd_pledged_real <- data2_success$usd_pledged_real[-train_ind1]
+
+# Fitting a polynomial model with cross validation
+library(boot)
+cv.error2 = rep(0, 7) #13840.66 for first degree
+for (i in 1:7) {
+  glm.fit = glm(usd_pledged_real~poly(backers, i)+poly(usd_goal_real, i)+poly(campLength, i)+X3D.Printing+Camera.Equipment+Fabrication.Tools+ Flight+Gadgets+Gaming.Hardware+Hardware+Product.Design+Sound+Tabletop.Games+Technology+Video.Games+Wearables, data = data2_success_normal)
+  cv.error2[i] = cv.glm(glmfit = glm.fit, data = data2_success_normal, K=10, cost = c)$delta[1]
+}
+plot(y=cv.error2,x=seq(1,7,by = 1), main = "K-Fold", type = "b")
+cv.error2
+
+# Fitting a generalized additive model (CV 14th 13061.06)
+cv.error3 = rep(0, 20)
+for (i in 1:20) {
+  gam1 <- gam(usd_pledged_real~bs(backers,df = i)+bs(usd_goal_real,df=i)+bs(campLength,df = i)+X3D.Printing+Camera.Equipment+Fabrication.Tools+ Flight+Gadgets+Gaming.Hardware+Hardware+Product.Design+Sound+Tabletop.Games+Technology+Video.Games+Wearables,data = data2_success_normal)
+  cv.error3[i] = cv.glm(glmfit = gam1, data = data2_success_normal, K=10, cost = c)$delta[1]
+}
+plot(y=cv.error3,x=seq(1,20,length.out = 20), main = "K-Fold", type = "b")
+cv.error3
+which.min(cv.error3)
+length(cv.error3)
 
 
 
